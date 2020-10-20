896c59db4fdec84503266e0cd0101385b78e2d89
Ryan JT <ryantalbot7@gmail.com>
2020-10-19 21:07:15 -0500

    Add made my ryanjt.dev on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import dev from '../images/dev.png';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        &#169; VW-Media.com
-      </div>
+      <>
+        <div className="footer">
+          &#169; VW-Media.com
+        </div>
+        <div className="footer">
+          <img className="footerImage" src={dev} alt=".dev logo" height={20} width={20} /> 
+          <span className="footerSpan">made by RyanJT.dev</span>
+        </div>
+      </>
     )
   }
 }
